5d6d125c3d53176c90a19192d37cfce7a8a81f5a
Nana Kwesi Baffoe-Mensah <NanaK@Nanas-MacBook-Pro-2.local>
2012-06-21 10:58:36 +0000

    latest change

--- UsingControlStructures.py
+++ UsingControlStructures.py
@@ -1,4 +1,4 @@
-
+#question 5
 theInput = raw_input("Enter integer:")
 theInput = int(theInput)
 if theInput%2 == 0:
@@ -25,6 +25,39 @@ elif personsAge >= presidentialage:
 elif personsAge >= officialretirementage:
     print "Too old"
 
+print"---------"
 
- 
+#question 7 using for
+for i in range(40,-1,-1):
+    if i%3 == 0:
+        print i
+print"solving question 7 using while loop"        
+
+ #using while for question 7
+i = 40
+while i >= 0:
+    i = i - 1
+    if i%3 == 0:
+        print i
+
+ #question 8
+print"---------"
+for i in range(6,30,1):
+    
+ if i%2 != 0 and i%3 != 0 and i%5 !=0:
+   print i
+
+print "--------------"
+
+#question 9
+n = 0
+while n >= 0:
+  n = n + 1  
+
+  if 79*n%97 == 1:
+   print n
+   break
+    
+
+
 
